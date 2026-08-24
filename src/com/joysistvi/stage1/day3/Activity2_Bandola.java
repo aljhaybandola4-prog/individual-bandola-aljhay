538837f80ee5ae97902b799b33edd4eeff90dd2b
Al Jhay <aljhaybandola4@gmail.com>
2026-08-24 20:24:24 +0800

    activity 2 expense tracker

--- src/com/joysistvi/stage1/day3/Activity2_Bandola.java
+++ src/com/joysistvi/stage1/day3/Activity2_Bandola.java
@@ -3,6 +3,33 @@ package com.joysistvi.stage1.day3;
 public class Activity2_Bandola {
 
     public static void main(String[] args) {
+        double income = 30000;
+        double foodallow = 1000;
+        double transportation = 2000;
+        double rent = 6000;
+        double utilityBill = 3500;
 
+        System.out.println("Income: " + income);
+
+        // percentage of food
+        double percentageFood = foodallow / income * 100;
+        System.out.printf("Food Allowance: %.1f %%", percentageFood);
+
+        // percentage of transportation
+        double percentageTranspo = transportation / income * 100;
+        System.out.printf("\nTransportation: %.1f %%", percentageTranspo);
+
+        // percentage of rent
+        double percentageRent = rent / income * 100;
+        System.out.printf("\nRent: %.1f %%", percentageRent);
+
+        // percentage of utility bill
+        double percentageUtility = utilityBill / income * 100;
+        System.out.printf("\nUtility Bill: %.1f %%", percentageUtility);
+
+        // remaining balance
+        double totalExpenses = foodallow + transportation + rent + utilityBill;
+        double balance = income - totalExpenses;
+        System.out.println("\nRemaining Balance: " + balance);
     }
 }
