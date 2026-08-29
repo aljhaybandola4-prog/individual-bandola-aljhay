adacc6c9ddf0fbb2ee8b47651b05d955521b6c92
Al Jhay <aljhaybandola4@gmail.com>
2026-08-29 13:01:08 +0800

    single dimension array

---
+++ src/com/joysistvi/stage1/day10/Activity8_Bandola.java
@@ -0,0 +1,50 @@
+package com.joysistvi.stage1.day10;
+import java.util.Scanner;
+
+public class Activity8_Bandola {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+        int sum = 0;
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+
+            sum += numbers[i];
+        }
+
+        System.out.println("\nYou entered:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Index " + i + ": " + numbers[i]);
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("\nSum = " + sum);
+        System.out.println("Average = " + average);
+
+        //Highest and Lowest
+        int highest = numbers[0];
+        int lowest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > highest) {
+                highest = numbers[i];
+            }
+
+            if (numbers[i] < lowest) {
+                lowest = numbers[i];
+            }
+        }
+
+        System.out.println("Highest = " + highest);
+        System.out.println("Lowest = " + lowest);
+
+        sc.close();
+    }
+}
